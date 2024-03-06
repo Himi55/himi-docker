d6062a3878293b6167a7ac00e7dc6a305e34eb4d
Himi55 <himanshisagar55@gmail.com>
2024-03-06 23:10:11 +0530

    uncomment celery task

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import UserContactDetails , UserRegisterModel
 
+
+
+
+
+
 admin.site.register(UserContactDetails)
 admin.site.register(UserRegisterModel)
